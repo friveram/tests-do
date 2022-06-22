fa0f79188f3160db9054fc0524f91f7f11811562
friveram <farivera.monzon@gmail.com>
2022-06-21 18:17:49 -0700

    test provision

--- script.sh
+++ script.sh
@@ -3,4 +3,7 @@
 yum install python -y
 yum install epel-release -y
 yum install ansible -y
+yum install git -y
 
+git clone https://github.com/friveram/tests-do.git
+ansible-playbook tests-do/playbooks/wordpress_playbook.yml
