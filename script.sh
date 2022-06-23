f0c6dde0974cedc42c7e76b04ac868a323929b55
friveram <farivera.monzon@gmail.com>
2022-06-22 19:07:53 -0700

    testing monitoring

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@ yum install git -y
 git clone https://github.com/friveram/tests-do.git
 ansible-playbook tests-do/playbooks/wordpress_playbook.yml
 
-echo ""
+DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=0913cc4f2c24c1932d5e4db42f047a4d DD_SITE="us5.datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
